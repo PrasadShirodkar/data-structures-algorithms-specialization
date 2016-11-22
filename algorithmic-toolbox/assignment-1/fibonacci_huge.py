cb17a6b52f6c4bbc18ce88cccac700c88deab2cb
Prasad Shirodkar <pras.shirodkar@gmail.com>
2016-11-21 20:31:42 -0500

    Submitted to autograder system. Successfully graded.

--- algorithmic-toolbox/assignment-1/fibonacci_huge.py
+++ algorithmic-toolbox/assignment-1/fibonacci_huge.py
@@ -1,17 +1,6 @@
 # Uses python3
-import random
 
-def get_fibonacci_huge_naive(n, m):
-    if n <= 1:
-        return n
-
-    previous = 0
-    current  = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-
-    return current % m
+import sys
 
 def get_pisano_period(n, m):
 
@@ -29,13 +18,9 @@ def get_pisano_period(n, m):
     return pisano_period
 
 
-def get_fibonacci_huge_modulo_pisano(n, m):
-
-    if n not in range(1, pow(10, 18) + 1):
-        raise ValueError("Value of n is not in the valid range from 1 to 10^18.")
-
-    if m not in range(2, pow(10, 5) + 1):
-        raise ValueError("Value of m is not in the valid range from 1 to 10^5.")
+def get_fibonacci_huge_pisano_modulo(n, m):
+    if n <= 1:
+        return n
 
     pisano_period = get_pisano_period(n, m)
 
@@ -45,24 +30,6 @@ def get_fibonacci_huge_modulo_pisano(n, m):
 
 
 if __name__ == '__main__':
-
-    while True:
-        # Generate random integer n between 1 and 10 to the power 3 both inclusive
-        max = pow(10, 3)
-        n = random.randint(1, max)
-
-        # Generate random integer n between 2 and 10 to the power 2 both inclusive
-        max = pow(10, 2)
-        m = random.randint(2, max)
-
-        print("n = " + str(n) + " m = " + str(m))
-
-        fibonacci_huge_naive = get_fibonacci_huge_naive(n, m)
-        print("Naive result for Fibonacci " + str(n) + " modulo " + str(m) + " is " + str(fibonacci_huge_naive))
-
-        fibonacci_huge_modulo_pisano = get_fibonacci_huge_modulo_pisano(n, m)
-        print("Pisano result Fibonacci " + str(n) + " modulo " + str(m) + " is " + str(fibonacci_huge_modulo_pisano))
-
-        if fibonacci_huge_naive != fibonacci_huge_modulo_pisano:
-            print("Solution failed. Please correct the solution")
-            break
+    input = sys.stdin.read()
+    n, m = map(int, input.split())
+    print(get_fibonacci_huge_pisano_modulo(n, m))
