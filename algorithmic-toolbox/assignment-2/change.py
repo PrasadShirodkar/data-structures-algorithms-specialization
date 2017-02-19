1a21f9d2fb5702d6f2179b49360427a2875e9562
Prasad Shirodkar <pras.shirodkar@gmail.com>
2017-02-19 15:16:36 -0500

    Solution for problem #1 Changing Money.

--- algorithmic-toolbox/assignment-2/change.py
+++ algorithmic-toolbox/assignment-2/change.py
@@ -5,18 +5,17 @@ import sys
 def process_change(amount, denomination):
     coins = int(amount / denomination)
     amount %= denomination
-    return [amount, coins]
+    return amount, coins
 
 
 def get_change(m):
     minimum_coins = 0
+    denominations = [10, 5, 1]
     while m > 0:
-        if m >= 10:
-            process_change(m, 10)
-        elif m >= 5:
-            process_change(m, 5)
-        elif m >= 1:
-            process_change(m, 1)
+        for item in denominations:
+            if m >= item:
+                m, coins = process_change(m, item)
+                minimum_coins += coins
     return minimum_coins
 
 if __name__ == '__main__':
